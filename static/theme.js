bb55c027e57adb8a66817f1e5b53cfad89f86e9c
Telegin <telegin.zhenya@gmail.com>
2018-06-09 13:16:14 +0600

    feat(theme): add dark mode

--- static/theme.js
+++ static/theme.js
@@ -1,12 +1,33 @@
+import { blendColors } from "../utils/blendColors";
+
 const theme = {
   day: {
     font: "'Ubuntu Mono', monospace",
     colors: {
+      backgroundColor: "#fffffff",
       white: "#ffffff",
+      grey: "#8c8c8c",
+
       fontColor: "#04143a",
       gradientFirst: "#ff7676",
       gradientSecond: "#f54ea2",
-      gradientMiddle: "#ff7676",
+
+      primaryColor: blendColors("#ff7676", "#f54ea2", 0.5),
+      gradient: `linear-gradient(45deg, #ff7676 0%, #f54ea2 100%)`
+    }
+  },
+  dark: {
+    font: "'Ubuntu Mono', monospace",
+    colors: {
+      backgroundColor: "#292b36",
+      white: "#ffffff",
+      grey: "#8c8f96",
+
+      fontColor: "#ffffff",
+      gradientFirst: "#ff7676",
+      gradientSecond: "#f54ea2",
+
+      primaryColor: blendColors("#ff7676", "#f54ea2", 0.5),
       gradient: `linear-gradient(45deg, #ff7676 0%, #f54ea2 100%)`
     }
   }
