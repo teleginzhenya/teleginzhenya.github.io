11d3ffca13bc7e557c0b7c1f029104d889e914ff
Telegin <telegin.zhenya@gmail.com>
2018-06-11 11:32:09 +0600

    fix(deploy): add .nojekyll to out directory

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ git branch -f master
 git checkout master
 git reset --hard origin/dev
 yarn export
+touch out/.nojekyll
 cp -r out/* .
 git add -A .
 git commit -a -m 'chore: deploy new production version'
