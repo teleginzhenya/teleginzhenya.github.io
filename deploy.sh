a860043859997b16612fc7502f21d1835cc55e48
Telegin <telegin.zhenya@gmail.com>
2018-06-11 11:36:46 +0600

    fix(deploy): add .nojekyll to root directory

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ git branch -f master
 git checkout master
 git reset --hard origin/dev
 yarn export
-touch out/.nojekyll
 cp -r out/* .
+touch .nojekyll
 git add -A .
 git commit -a -m 'chore: deploy new production version'
 git push origin master --force
