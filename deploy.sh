043875660e857a4d9da9f623a20b3d2b44ea159c
Telegin <telegin.zhenya@gmail.com>
2018-06-11 11:09:07 +0600

    feat: introduce deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git branch -f master
+git checkout master
+git reset --hard origin/dev
+yarn export
+cp -r out/* .
+git add -A .
+git commit -a -m 'chore: deploy new production version'
+git push origin master --force
+git checkout dev
